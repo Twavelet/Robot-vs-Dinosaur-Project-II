8f070c9141bd14d8e1716ca601d57c33fb3051ea
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-08-15 15:26:47 -0400

    Reorganized the code that was in the Run-game method into the Battle-phase method. Combining all the other methods including the battle-phase into the run-game method.

--- battlefield.py
+++ battlefield.py
@@ -9,29 +9,30 @@ class Battlefield:
     
 
     def run_game(self):
+        self.display_welcome
+        self.battle_phase
+
+    
+
+
+    def display_welcome(self):
+        print("Welcome to Fight Island! Enjoy the Show!")
+
+    def battle_phase(self):
         while True:
             if self.dinosaur.health > 0:
                 self.dinosaur.attack(self.robot)
             if self.robot.health > 0:
                 self.robot.attack(self.dinosaur)
             elif self.dinosaur.health <= 0:
-                print("The dinosaur's health has reached zero. The robot has won this battle!")
+                self.winner
+                break
             elif self.robot.health <= 0:
-                print("The robot's health has reached zero. The dinosaur has won this battle!")
-
-        #need to call the attack functions
-        #need the robot and dinosaur to alternate attacks
-        #need ot print out the attack weapon, damage done, health remaining after each attack
-        #need to display who won after health reaches 0
-        pass 
-
-
-    def display_welcome(self):
-        print("Welcome to Fight Island! Enjoy the Show!")
-
-    def battle(self):
-        pass
+                self.winner
+                break
 
     def winner(self):
-        print("The winner is:")
+        print(" ")
 
+
+#pretty sure my run game is supposed to be the battle phase and the run game is supposed to encompass everything
